e0efa55adf90df9bec70c5b42cd82491b38a1846
contactnotifieralex-jpg <contactnotifieralex@gmail.com>
2026-03-30 18:54:52 +0200

    Create autocomplete.py

---
+++ autocomplete.py
@@ -0,0 +1,7 @@
+# autocomplete.py - Versión mínima para evitar errores
+
+async def play_autocomplete(interaction, current):
+    return []
+
+async def playlists_autocomplete(interaction, current):
+    return []
